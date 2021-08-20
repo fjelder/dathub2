c109f674c4280eae607a77c472d384263379bca8
fjelder <mich.filipiak@gmail.com>
2021-08-20 12:31:03 +0200

    Change app file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
-
+<!-- Dodanie komentarza na potrzeby git'a -->
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
             <!-- Page Heading -->
