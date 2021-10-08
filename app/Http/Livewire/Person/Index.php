353a19943d3d1fc3aa9688b2ace60a4bb0bd05d6
fjelder <mich.filipiak@gmail.com>
2021-10-08 13:58:34 +0200

    add person livewire component

---
+++ app/Http/Livewire/Person/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Person;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.person.index');
+    }
+}
