572705f3410c240fa6c500dfb5a8d447eba32339
fjelder <mich.filipiak@gmail.com>
2021-10-08 14:30:26 +0200

    people index component in livewire

--- app/Http/Livewire/Person/Index.php
+++ app/Http/Livewire/Person/Index.php
@@ -3,11 +3,13 @@
 namespace App\Http\Livewire\Person;
 
 use Livewire\Component;
-
+use App\Models\Person;
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.person.index');
+        return view('livewire.person.index', [
+            'people' => Person::all()
+        ]);
     }
 }
