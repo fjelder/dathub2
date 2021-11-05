ea1d65fdfa0370ec2255fb55fad383731485595d
fjelder <mich.filipiak@gmail.com>
2021-11-05 14:38:51 +0100

    work 0511a

--- resources/views/date/railway-line/index.blade.php
+++ resources/views/date/railway-line/index.blade.php
@@ -13,12 +13,15 @@
 
 <div>
     <div class="md:grid md:grid-cols-3 md:gap-6">
-      <div class="md:col-span-1 sm:row-start-2">
-        @livewire('date.railway-line-crud', ['mode' => 'create'])
-      </div>
-      <div class="mt-5 md:mt-0 md:col-span-2 row-start-1">
+      
+      <div class="mt-5 md:mt-0 md:col-span-2 sm:row-start-1">
         @livewire('date.railway-line-table')
       </div>
+
+      <div class="md:col-span-1 sm:row-start-2 mt-10">
+        @livewire('date.railway-line-crud', ['mode' => 'create'])
+      </div>
+
     </div>
   </div>
   
